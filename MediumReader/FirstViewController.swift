198379b7236107576c9bfe963cbc075658b2050c
Preslav Rachev <rachev.preslav@gmail.com>
2017-07-07 23:20:04 +0200

    Initial Commit

---
+++ MediumReader/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  MediumReader
+//
+//  Created by Preslav Rachev on 07/07/17.
+//  Copyright © 2017 Preslav Rachev. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
